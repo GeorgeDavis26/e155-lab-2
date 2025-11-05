8c48eca188855035c08bff2f7eba02b7f0db021d
PorridgeGeorge <gdavis@g.hmc.edu>
2025-11-05 15:36:39 -0800

    finally resubmitting

--- fpga/src/switch_mux_tb.sv
+++ fpga/src/switch_mux_tb.sv
@@ -1,4 +1,4 @@
-// swtich_encoder_tb.sv
+// swtich_mux_tb.sv
 // testbench file for the switch_encoder
 // george davis gdavis@hmc.edu
 // 8/31/2025
@@ -7,68 +7,50 @@
 
 	`timescale 1ps/1ps //timescale <time_unit>/<time_precision>
 
-module switch_mux; 
-	
-	logic	clk;
-	logic	reset;
+module switch_mux_tb; 
 	
 	//input/output variables
     logic           enable;
 	logic	[3:0]   sA, sB;
-	logic   [3:0]   s, s_expected;
+	int     		a, b;
+	logic   [3:0]   s;
 
 	//32 bit vectornum indicates the number of test vectors applied
 	//32 bit errors indicates number of errros found
-	logic	[31:0]	vectornum, errors;
-	logic	[11:0]	testvectors[10000:0]; //change bit length to match DUT input/output
+	logic	[31:0] errors;
 	
 	//instatiate device to be tested
-	switch_mux dut(enable, sA, sB, s); //change to DUT input/output
+	switch_mux dut(enable, sA, sB, s); 
 
-    //generates clock 
-	always
-		begin
-			clk <= 1; # 5; clk <= 0; # 5;
-		end
-
-	
 	initial
-		begin
-			$readmemb("switch_mux.tv", testvectors); //change to DUT .tv file
-			
-			//Initialize 0 vectors tested and errors
-			vectornum = 0;
-			errors = 0; 
-			
-			//Pulse reset to begin test
-			reset <= 1; # 22; reset <= 0;
-		end
+		errors = 0;
 
-	  	//Check test vectors at the falling edge of the clock 
-		always @(posedge clk)
-			begin
-				#1;
-				//loads test vectors into inputs and expected outputs
-				{sA, sB ,s_expected} = testvectors[vectornum]; //change to DUT input/output
-			end
-	
-    
-		always @(negedge clk)
-			if(~reset) begin
-				//detect error by comparing actual output expected output from testvectors
-				if (s != s_expected) begin //change to DUT output
-					//display input/outputs that generated the error
-					$display("Error: inputs = %b", {in});   //change to DUT input
-					$display(" outputs = %b", {out});       //change to DUT output
+		for (a = 0; a < 16; a = a + 1) begin
+			for(b = 0; b < 16; b = b + 1)begin
+
+				sA = a;
+				sB = b;
+				#1
+				enable = 0;
+				assert(s == sA) else begin
+					$error("Error: inputs = %b", {sA, sB}); 
+					$error(" outputs = %b", {s});
 					errors = errors + 1;
 				end
+				#5
+				enable = 1;
+				assert(s == sB) else begin
+					$error("Error: inputs = %b", {sA, sB}); 
+					$error(" outputs = %b", {s});
+					errors = errors + 1;
+				end
+				#4
 
-				vectornum = vectornum + 1;
-				
-				if (testvectors[vectornum] == 12'bX) begin   //change bit length to DUT input/output combined
-					$display("%d tests completed with %d errors", vectornum, errors);
+				if(a == 15 && b == 15)begin
+					$error("%d tests completed with %d errors", (a*b), errors);
 					$stop;
 				end
 			end
+		end
 
 endmodule
